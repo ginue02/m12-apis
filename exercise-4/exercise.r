dea56f759e73c30133669f0ae63e90ad94c42928
Ginue <ginue02@uw.edu>
2017-04-26 17:15:54 -0700

    completed most of the problems in lab section

--- exercise-4/exercise.r
+++ exercise-4/exercise.r
@@ -1,23 +1,25 @@
 # Install and load the jsonlite package
+install.packages("httr")
+install.packages("jsonlite")
 library(jsonlite)
 library(httr)
 
 # Make a variable base.url that has the same base url from the omdb documentation.
 # (Hint: visit https://www.omdbapi.com/ to find the base url)
-
+base.url <- "http://www.omdbapi.com/?"
 
 # Make a variable called movie that has the name of your favorite movie
-
+movie <- "Captain America: Civil War"
 
 # Make a variable called "query.parameters" that holds a list of the parameters
 # to pass to the API. View the OMDb documentation to see which parameters
 # are available.
-
+query.parameters <- list(type = movie, plot = "short", r = "json")
 
 # Make a variable called request that is a string of a request URL made up of the base URL
 # and the parameters string
-
-
+response <- GET(base.url, query = query.parameters)
+body <- content(response, "text")
 # Use fromJSON to retrieve JSON data from the omdb api using your request.
 # Store the result in a variable called movie.data
 
